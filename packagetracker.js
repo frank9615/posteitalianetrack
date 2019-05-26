6aa400bd9f664f967c17eff3f002f574e6080d7c
Francesco Accardi <accardifrancesco@hotmail.it>
2019-05-26 21:21:57 +0200

    Update packagetracker.js

--- packagetracker.js
+++ packagetracker.js
@@ -1,23 +1,29 @@
 /*   POSTE ITALIANE STATUS CODE TRACK*/
+var request = require('sync-request');
+var sleep = require('thread-sleep');
+const notifier = require('node-notifier');
+const path = require('path');
 
-const request = require('request')
-
-function getTrack(code){
-    request.post(' https://www.poste.it/online/dovequando/DQ-REST/ricercasemplice', {
-    json: {
-        tipoRichiedente:'WEB',
-        codiceSpedizione:code,
-        periodoRicerca:1
-    }
-    }, (error, res, body) => {
-        var json_track = JSON.stringify(body);
-        track = JSON.parse(json_track);
-        console.log(resolveStatusCode(track.stato));
+function getTrack(idtrack){
+    var res = request('POST', 'https://www.poste.it/online/dovequando/DQ-REST/ricercasemplice', {
+        json: {
+            tipoRichiedente:'WEB',
+            codiceSpedizione:idtrack,
+            periodoRicerca:1
+        }
     });
+    var json_track = res.getBody('utf8');
+    track = JSON.parse(json_track);
+    return track;
 }
-
+function getStato(track){
+    if(track){
+        return parseInt(track.stato);
+    }
+}
 function resolveStatusCode(code){
-    switch(parseInt(code)){
+    console.log(code);
+    switch(code){
         case 2: return "Presa in Carico";
         case 3: return "In Transito";
         case 4: return "In Consegna";
@@ -25,4 +31,43 @@ function resolveStatusCode(code){
         default: return "Impossible to resolve Status Code";
     }
 }
+function checkValidity(track){
+    var res = track.esitoRicerca;
+    if(res == 3){
+        return true;
+    }
+    return false;
+}
 
+function mynotify(message){
+    notifier.notify(
+        {
+          title: "Poste Italiane Tracker Package",
+          message: message,
+          icon: path.join(__dirname, 'truck_icon.ico'), 
+          sound: true
+        });
+}
+
+function main(){
+    var idtrack = process.argv[2];
+    var track = getTrack(idtrack);
+    if(!checkValidity(track)){
+        console.log("Error, Invalid Code");
+        return ;
+    }
+    var statuscode = getStato(track);
+    while(statuscode!=5){
+        sleep(10000); //update every 10 minute
+        var code = getStato(getTrack(idtrack));
+        if(code!=statuscode ){
+            mynotify(resolveStatusCode(code));
+            statuscode=code;
+        } 
+    }
+    if (statuscode==5){
+        mynotify(resolveStatusCode(statuscode));
+    }
+}
+
+main();
